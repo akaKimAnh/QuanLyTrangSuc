5ff79b2e58142dce51f16b1ea97170d9e1b64433
akakimanh <kimanh.220102@gmail.com>
2023-07-07 20:20:48 +0700

    Completed

---
+++ admin/php/server.php
@@ -0,0 +1,9 @@
+<?php
+$hostname = '127.0.0.1';
+$username = 'kimanh';
+$password = 'kimanh+';
+$dbname = "tnat";
+$port = 3306;
+$conn = mysqli_connect($hostname, $username, $password,$dbname,$port);
+
+
